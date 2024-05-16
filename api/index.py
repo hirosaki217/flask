e5c53e0ae8dc7dbe056498c0a3ef9b047d453300
hieu <hirosaki217@gmail.com>
2024-05-16 17:11:35 +0700

    no

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 import sys
-# import undetected_chromedriver as uc
 # from selenium.webdriver.common.by import By
 # from webdriver_manager.chrome import ChromeDriverManager
 # ChromeDriverManager().install()
@@ -17,28 +16,29 @@ def about():
     python_version = f"{sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}"
     return 'About - Python Version: ' + python_version
 
-# @app.route('/housenow-crawl', methods=['POST'])
-# def home2():
-#     url = request.json.get('url')
-#     data = get_page_source(url)
-#     return {
-#         'html': data
-#     }
+@app.route('/housenow-crawl', methods=['POST'])
+def home2():
+    import undetected_chromedriver as uc
+    url = request.json.get('url')
+    my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
+  
+    options = uc.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument(f"user-agent={my_user_agent}")
+  
+    # Initialize Chrome WebDriver with the specified options
+    driver = uc.Chrome(options=options)
+    driver.get(url)
+    page_source = driver.page_source
+    driver.quit()
+    return {
+        'html': page_source
+    }
     
     
 
 
 def get_page_source(url):
   # Define a custom user agent
-  my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
   
-  options = uc.ChromeOptions()
-  options.add_argument("--headless")
-  options.add_argument(f"user-agent={my_user_agent}")
-  
-  # Initialize Chrome WebDriver with the specified options
-  driver = uc.Chrome(options=options)
-  driver.get(url)
-  page_source = driver.page_source
-  driver.quit()
   return page_source
