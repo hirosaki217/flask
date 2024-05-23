37338913d2765656936edab5210330053525679b
hieu <hirosaki217@gmail.com>
2024-05-23 15:37:43 +0700

    x

--- api/index.py
+++ api/index.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from tempfile import mkdtemp
 from flask import Flask,request
+import os
 app = Flask(__name__)
  
 
@@ -19,21 +20,22 @@ def test():
 def get_uc_driver():
         
     options = uc.ChromeOptions()
-    options.add_argument('--no-sandbox')
+    # options.add_argument('--no-sandbox')
     options.add_argument("--disable-gpu")
     options.add_argument("--single-process")
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument(f"--user-data-dir={mkdtemp()}")
     options.add_argument(f"--data-path={mkdtemp()}")
     options.add_argument(f"--disk-cache-dir={mkdtemp()}")    
-    ChromeDriverManager(driver_version="125").install()
-    # driver_executable_path = '/tmp/undetected_chromedriver/undetected_chromedriver'
+    driver_executable_path = ChromeDriverManager(driver_version="125", url="https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.76/linux64/chromedriver-linux64.zip").install()
     # os.system(f'cp /opt/chromedriver {driver_executable_path}')
-    # os.chmod(driver_executable_path, 0o777)
+    os.chmod(driver_executable_path, 0o777)
 
-    driver = uc.Chrome( options=options, headless=True, no_sandbox=True)#, use_subprocess=True)
+    driver = uc.Chrome( options=options, driver_executable_path= driver_executable_path, headless=True, version_main=125)#, use_subprocess=True)
 
     return driver  
 
 
-
+# driver = get_uc_driver()
+# driver.get('http://checkip.amazonaws.com/')
+# print(driver.page_source)
